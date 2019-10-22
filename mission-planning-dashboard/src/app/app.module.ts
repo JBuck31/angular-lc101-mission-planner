203b6d5358559e56acb64b530f46967bbb69f1f8
Jen Buck <jenbuck@Chriss-MacBook-Pro.local>
2019-10-21 19:27:49 -0500

    finished the studio

--- mission-planning-dashboard/src/app/app.module.ts
+++ mission-planning-dashboard/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { Crew } from './crew/crew.component';
+import { CrewComponent } from './crew/crew.component';
+import { EquipmentComponent } from './equipment/equipment.component';
+import { ExperimentsComponent } from './experiments/experiments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CrewComponent,
+    CrewComponent,
+    EquipmentComponent,
+    ExperimentsComponent
   ],
   imports: [
     BrowserModule
